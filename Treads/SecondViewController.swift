3822d9db0e7c9f4af71a653fe2a99dd3daf3d2b9
Odhran Daly <odhran.daly25@mail.dcu.ie>
2019-08-08 22:12:21 +0100

    Initial Commit

---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Odhrán Daly on 08/08/2019.
+//  Copyright © 2019 Odhrán Daly. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
