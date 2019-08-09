974c9c91548a8e4ae98a3bd1418abba82e1f135d
Odhran Daly <odhran.daly25@mail.dcu.ie>
2019-08-09 22:54:18 +0100

    Map vew and location point

---
+++ Treads/Controller/LocationVC.swift
@@ -0,0 +1,29 @@
+//
+//  LocationVC.swift
+//  Treads
+//
+//  Created by Odhrán Daly on 09/08/2019.
+//  Copyright © 2019 Odhrán Daly. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, MKMapViewDelegate {
+    
+    var manager: CLLocationManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+        
+    }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+        }
+    }
+}
